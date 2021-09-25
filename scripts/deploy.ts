c6c1f57e906c4e216f15c3033b178056cba98b45
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2021-09-25 12:48:20 -0400

    lesson 5

---
+++ scripts/deploy.ts
@@ -0,0 +1,29 @@
+import { ethers } from 'hardhat'
+import { WavePortal, WavePortal__factory } from '../typechain'
+
+const main = async () => {
+  const [deployer] = await ethers.getSigners()
+  const accountBalance = await deployer.getBalance()
+
+  console.log('Deploying contracts with account: ', deployer.address)
+  console.log('Account balance: ', accountBalance.toString())
+
+  const Token: WavePortal__factory = await ethers.getContractFactory(
+    'WavePortal',
+  )
+  const portal: WavePortal = await Token.deploy()
+
+  console.log('WavePortal address: ', portal.address)
+}
+
+const runMain = async () => {
+  try {
+    await main()
+    process.exit(0)
+  } catch (error) {
+    console.error(error)
+    process.exit(1)
+  }
+}
+
+runMain()
