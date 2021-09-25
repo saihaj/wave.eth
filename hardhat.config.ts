406184279198c14997d32532a30813d3498930c6
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2021-09-25 15:53:57 -0400

    setup testnet deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,3 +1,5 @@
+import { config } from 'dotenv'
+config() // Load env variables
 import { task } from 'hardhat/config'
 import '@nomiclabs/hardhat-waffle'
 import '@typechain/hardhat'
@@ -17,6 +19,12 @@ task('accounts', 'Prints the list of accounts', async (_, hre) => {
  */
 module.exports = {
   solidity: '0.8.4',
+  networks: {
+    rinkeby: {
+      url: 'https://eth-rinkeby.alchemyapi.io/v2/E27pYQjb9MZQ55BvxLe4TAnTIrlPY49T',
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
   typechain: {
     outDir: 'typechain',
     target: 'ethers-v5',
