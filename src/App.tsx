7270094d0a15103b5123f8b7ab40cace08e758ea
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2021-09-26 12:52:17 -0400

    make it wave

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { ethers } from 'ethers'
+import Reward from 'react-rewards'
+import Loader from 'react-loader-spinner'
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+
+import { WavePortal } from '../typechain'
 import './App.css'
+import CONTRACT_ABI from './WavePortal.json'
+
+const CONTRACT_ADDRESS = '0xf77D3817B7b66746580c0F9912cd24Ad9b085217'
 
 const App = () => {
   const [account, setAccount] = useState()
+  const [pendingTransaction, setPendingTransaction] = useState(false)
+  const waveBtn = useRef()
 
   const checkIfWalletIsConnected = async () => {
     //   @ts-ignore
@@ -46,6 +56,48 @@ const App = () => {
     }
   }
 
+  const wave = async () => {
+    try {
+      // @ts-ignore
+      const { ethereum } = window
+
+      if (!ethereum) {
+        console.log("Ethereum object doesn't exist!")
+        return
+      }
+
+      const provider = new ethers.providers.Web3Provider(ethereum)
+      const signer = provider.getSigner()
+      const contract = new ethers.Contract(
+        CONTRACT_ADDRESS,
+        CONTRACT_ABI.abi,
+        signer,
+      ) as WavePortal
+
+      let count = await contract.getTotalWaves()
+      console.log('Retrieved total wave count...', count.toNumber())
+
+      setPendingTransaction(true)
+      const waveTxn = await contract.wave()
+      console.log('Mining...', waveTxn.hash)
+      await waveTxn.wait()
+      console.log('Mined!', waveTxn.hash)
+      setPendingTransaction(false)
+      // @ts-ignore
+      waveBtn?.current.rewardMe()
+      setTimeout(() => {
+        window.location.reload()
+      }, 3000)
+
+      count = await contract.getTotalWaves()
+      console.log('Retrieved total wave count...', count.toNumber())
+    } catch (error) {
+      // @ts-ignore
+      waveBtn?.current.punishMe()
+      console.log(error)
+    }
+  }
+
   // This runs our function when the page loads.
   useEffect(() => {
     checkIfWalletIsConnected()
@@ -55,17 +107,16 @@ const App = () => {
     <main className="home">
       <h1>Hey there</h1>
       <h3>I am Saihaj. Connect your Ethereum wallet and wave at me.</h3>
-
       {account ? (
-        <button
-          onClick={() => {
-            console.log('wave')
-          }}
-        >
-          Wave at me
-        </button>
+        <Reward ref={waveBtn} type="memphis">
+          {pendingTransaction ? (
+            <Loader width={60} type="Circles" color="var(--light-primary)" />
+          ) : (
+            <button onClick={wave}>Wave at me</button>
+          )}
+        </Reward>
       ) : (
-        <button className="waveButton" onClick={connectWallet}>
+        <button ref={waveBtn} className="waveButton" onClick={connectWallet}>
           Connect Wallet
         </button>
       )}
